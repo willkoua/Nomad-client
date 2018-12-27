eb5a88a82aa51608155d761b0fbebb0280d44ef0
willy Kouagnia <willkoua@gnail.com>
2018-12-27 18:47:31 -0500

    first code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { HeaderComponent } from './header/header.component';
+
+import {Routes, RouterModule} from '@angular/router';
+import {AuthService} from './services/auth.service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import * as $ from 'jquery';
+
+const appRoutes: Routes = [
+  { path: 'auth/signin', component: SigninComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    SigninComponent,
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
